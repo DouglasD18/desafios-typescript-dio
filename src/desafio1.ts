b0c09aa664da707237e352cca7cd439f0656dbe4
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-06-22 00:15:49 -0300

    Resolução dos desafios.

---
+++ src/desafio1.ts
@@ -0,0 +1,10 @@
+// Código para ser refatorado.
+
+/* let employee = {};
+employee.code = 10;
+employee.name = "John"; */
+
+const employee: { code: number, name: string } = {
+  code: 10,
+  name: "John"
+}
